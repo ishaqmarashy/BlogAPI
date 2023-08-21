fca358720f2d93740afdfcf3d2a8a7c9fa2d7953
ishaqmarashy <34040118+ishaqmarashy@users.noreply.github.com>
2023-08-22 01:52:55 +0400

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,6 +1,6 @@
 const Mongoose = require("mongoose");
 const Schema = Mongoose.Schema;
-const mongoDb =`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@test-server.dduiwsv.mongodb.net/`;
+const mongoDb =process.env.MONGO_CON;
 const jwt = require('jsonwebtoken');
 const debug=require('debug')('model');
 
